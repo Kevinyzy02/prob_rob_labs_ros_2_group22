c6853a4477a2a5e7831509b84aaffacada54c040
Kevinyzy02 <kevinyzy02@outlook.com>
2025-09-25 20:23:44 -0400

    lab 2

--- prob_rob_labs/setup.py
+++ prob_rob_labs/setup.py
@@ -33,6 +33,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'lab_2_question_4 = lab_2_question_4.lab_2_question_4:main',
+            'lab_2_question_3 = lab_2_question_3.lab_2_question_3:main',
             'image_mean_feature_x = image_mean_feature_x.image_mean_feature_x:main',
             'flaky_door_opener = flaky_door_opener.flaky_door_opener:main',
         ],
