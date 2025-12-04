828cdc59f3fafe78408736aefdcb78cdab427361
Kevinyzy02 <kevinyzy02@outlook.com>
2025-12-04 17:23:13 -0500

    6.1

--- prob_rob_labs/launch/lab_6_assignment_1_launch.py
+++ prob_rob_labs/launch/lab_6_assignment_1_launch.py
@@ -1,19 +1,36 @@
 import os
-
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
+    package_name = 'prob_rob_labs'
+    try:
+        pkg_share = get_package_share_directory(package_name)
+        default_map_path = os.path.join(pkg_share, 'config', 'lab6_landmarks.yaml')
+    except Exception as e:
+        print(f"Error finding package share directory: {e}")
+        default_map_path = ''
+
     return LaunchDescription([
-        DeclareLaunchArgument('use_sim_time', default_value='true',
-                              description='set to true for simulation'),
+        DeclareLaunchArgument(
+            'use_sim_time', 
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
+        DeclareLaunchArgument(
+            'map_file',
+            default_value=default_map_path,
+            description='Full path to the landmarks YAML file'),
         Node(
-            package='prob_rob_labs',
-            executable='lab_6_assignment_1',
+            package=package_name,
+            executable='lab_6_assignment_1', 
             name='lab_6_assignment_1',
-            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+            output='screen',
+            parameters=[{
+                'use_sim_time': LaunchConfiguration('use_sim_time'),
+                'map_file': LaunchConfiguration('map_file')
+            }]
         )
     ])
--- prob_rob_labs/src/lab_6_assignment_1/lab_6_assignment_1.py
+++ prob_rob_labs/src/lab_6_assignment_1/lab_6_assignment_1.py
@@ -1,63 +1,135 @@
 #!/usr/bin/env python3
-
-import os
-import yaml
-
 import rclpy
 from rclpy.node import Node
+import yaml
+import math
+import numpy as np
+from functools import partial
 
+from sensor_msgs.msg import CameraInfo
+from std_msgs.msg import String, Float64
 
-heartbeat_period = 0.1
+from prob_rob_msgs.msg import Point2DArrayStamped
 
 class Lab_6Assignment_1(Node):
-
     def __init__(self):
         super().__init__('lab_6_assignment_1')
-        self.declare_parameter("map_file", "config/lab6_landmarks.yaml")
-        map_file_param = self.get_parameter("map_file").get_parameter_value().string_value
+
+        self.declare_parameter('map_file', 'lab6_landmarks.yaml')
         
-        this_dir = os.path.dirname(os.path.abspath(__file__))   # .../prob_rob_labs/src/lab_6_assignment_1
-        src_dir = os.path.dirname(this_dir)                     # .../prob_rob_labs/src
-        pkg_root = os.path.dirname(src_dir)                     # .../prob_rob_labs
+        self.map_data = {}
+        self.camera_params = {
+            'fx': None,
+            'fy': None,
+            'cx': None,
+            'cy': None,
+            'received': False
+        }
+        
+        self.load_map()
 
-        if os.path.isabs(map_file_param):
-            map_file = map_file_param
-        else:
-            map_file = os.path.join(pkg_root, map_file_param)
+        self.create_subscription(CameraInfo, '/camera/camera_info', self.camera_info_callback, 10)
 
-        if not os.path.exists(map_file):
-            self.get_logger().fatal(f"Map file does not exist: {map_file}")
-            raise SystemExit(1)
-
-        self.get_logger().info(f"Loading landmark map from: {map_file}")
-        with open(map_file, "r") as f:
-            data = yaml.safe_load(f)
-
-        self.landmarks = data.get("landmarks", {})
-
-        if not self.landmarks:
-            self.get_logger().warn("No landmarks found in map file!")
-
-        for color, info in self.landmarks.items():
-            x = info.get("x")
-            y = info.get("y")
-            w = info.get("width")
-            h = info.get("height")
-            frame_id = info.get("frame_id", "map")
-
-            self.get_logger().info(
-                f"Landmark '{color}': "
-                f"x={x}, y={y}, width={w}, height={h}, frame_id={frame_id}"
+        self.landmark_subs = []
+        for color, data in self.map_data.items():
+            topic_name = data['topic']
+            self.get_logger().info(f"Subscribing to {color} landmark at {topic_name}")
+            
+            sub = self.create_subscription(
+                Point2DArrayStamped,
+                topic_name,
+                partial(self.landmark_callback, landmark_color=color),
+                10
             )
+            self.landmark_subs.append(sub)
 
+        self.get_logger().info("EKF Localization Node Initialized")
 
-def main(args=None) -> None:
+    def load_map(self):
+        map_path = self.get_parameter('map_file').get_parameter_value().string_value
+        
+        try:
+            with open(map_path, 'r') as file:
+                data = yaml.safe_load(file)
+            for item in data['landmarks']:
+                self.map_data[item['color']] = item
+                
+            self.get_logger().info(f"Loaded {len(self.map_data)} landmarks from {map_path}")
+            
+        except Exception as e:
+            self.get_logger().error(f"Failed to load map file: {e}")
+
+    def camera_info_callback(self, msg):
+        if not self.camera_params['received']:
+            P = msg.p
+            self.camera_params['fx'] = P[0]
+            self.camera_params['fy'] = P[5]
+            self.camera_params['cx'] = P[2]
+            self.camera_params['cy'] = P[6]
+            self.camera_params['received'] = True
+            self.get_logger().info(f"Camera Params Received: fx={P[0]}, fy={P[5]}")
+
+    def calculate_variance(self, dist, bearing):
+
+        sigma_dist = 0.01 * dist**2 + 0.005
+        sigma_bearing = 0.001 * dist**2 + 0.003
+        
+        return sigma_dist, sigma_bearing
+
+    def landmark_callback(self, msg: Point2DArrayStamped, landmark_color: str):
+
+        if not self.camera_params['received'] or not msg.points:
+            return
+
+        xs = np.array([p.x for p in msg.points])
+        ys = np.array([p.y for p in msg.points])
+        
+        if len(msg.points) < 4:
+            return
+
+        y_min, y_max = np.min(ys), np.max(ys)
+        x_min, x_max = np.min(xs), np.max(xs)
+        
+        h_pix = y_max - y_min
+        
+        if h_pix <= 1.0:
+            return
+
+        lm_info = self.map_data[landmark_color]
+        real_height = lm_info['height']
+
+        x_p = (x_min + x_max) / 2.0
+        
+        fx = self.camera_params['fx']
+        cx = self.camera_params['cx']
+        
+        theta = math.atan((cx - x_p) / fx)
+
+        fy = self.camera_params['fy']
+        
+        d = (real_height * fy) / (h_pix * math.cos(theta))
+
+        var_dist, var_theta = self.calculate_variance(d, theta)
+
+        self.get_logger().info(
+            f"[{landmark_color.upper()}] "
+            f"d={d:.2f}m, "
+            f"theta={math.degrees(theta):.2f}deg, "
+            f"h_pix={h_pix:.1f}, "
+            f"var_dist={var_dist:.1f}, "
+            f"var_theta={var_theta:.1f}, "
+        )
+        
+
+def main(args=None):
     rclpy.init(args=args)
     node = Lab_6Assignment_1()
-    rclpy.spin(node)
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
     node.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
